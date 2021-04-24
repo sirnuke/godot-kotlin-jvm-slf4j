fd2618e2eb2c141745fcaef50b0f8b31a959ecd3
Bryan DeGrendel <bryan@degrendel.com>
2021-04-24 17:47:36 -0400

    Add more publishing config
    Should generate sources and javadoc jars, plus hopefully correct config for uploading to Sonatype.

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,11 +1,25 @@
 plugins {
   `java-library`
   `maven-publish`
+  signing
+  id("org.jetbrains.dokka") version "1.4.32"
 }
 
 dependencies {
   implementation("com.utopia-rise:godot-library:0.1.4-3.2.3")
   api("org.slf4j:slf4j-api:1.7.30")
+}
+
+java {
+  withSourcesJar()
+}
+
+val dokkaHtml by tasks.getting(org.jetbrains.dokka.gradle.DokkaTask::class)
+
+val javadocJar: TaskProvider<Jar> by tasks.registering(Jar::class) {
+  dependsOn(dokkaHtml)
+  archiveClassifier.set("javadoc")
+  from(dokkaHtml.outputDirectory)
 }
 
 if (project.properties.containsKey("internalMavenURL"))
@@ -18,6 +32,7 @@ if (project.properties.containsKey("internalMavenURL"))
     publications {
       create<MavenPublication>("maven") {
         from(components["java"])
+        artifact(javadocJar)
         artifactId = "godot-kotlin-jvm-slf4j"
       }
     }
@@ -31,6 +46,58 @@ if (project.properties.containsKey("internalMavenURL"))
         val snapshotsRepoUrl = uri("$internalMavenURL/snapshots/")
         url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
         name = "Internal-Maven"
+      }
+    }
+  }
+}
+else if (project.properties.containsKey("ossrhUsername"))
+{
+  val ossrhUsername: String by project
+  val ossrhPassword: String by project
+
+  signing {
+    sign(publishing.publications["maven"])
+  }
+
+  publishing {
+    publications {
+      create<MavenPublication>("maven") {
+        artifact(javadocJar)
+        pom {
+          name.set("Godot SLF4J Kotlin-JVM")
+          description.set("SLF4J logger for Godot's Kotlin (JVM) bindings")
+          url.set("https://github.com/sirnuke/godot-kotlin-jvm-slf4j")
+          licenses {
+            license {
+              name.set("MIT License")
+              url.set("https://github.com/sirnuke/godot-kotlin-jvm-slf4j/blob/main/LICENSE")
+            }
+          }
+          developers {
+            developer {
+              id.set("sirnuke")
+              name.set("Bryan DeGrendel")
+              email.set("bryan@degrendel.com")
+            }
+          }
+          scm {
+            connection.set("scm:git:https://github.com/sirnuke/godot-kotlin-jvm-slf4j.git")
+            developerConnection.set("scm:git:ssh:git@github.com:godot-kotlin-jvm-slf4j.git")
+            url.set("https://github.com/sirnuke/godot-kotlin-jvm-slf4j")
+          }
+        }
+      }
+    }
+    repositories {
+      maven {
+        credentials {
+          username = ossrhUsername
+          password = ossrhPassword
+        }
+        val releasesRepoUrl = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+        val snapshotsRepoUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+        url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+        name = "ossrh"
       }
     }
   }
